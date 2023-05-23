3497890f62e75fff805df317af3c6eb9d90a2611
AnastasiaYakushina <nastasya19.11@yandex.ru>
2023-05-23 20:18:50 +0300

    add formatter JSON

--- formatters/index.js
+++ formatters/index.js
@@ -1,9 +1,13 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const formatter = (result, type) => {
   if (type === 'plain') {
     return plain(result);
+  }
+  if (type === 'json') {
+    return json(result);
   }
   return stylish(result);
 };
