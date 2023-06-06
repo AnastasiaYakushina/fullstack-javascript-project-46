ed8ea68efa6b10803bd7104a81de29309fa884ee
AnastasiaYakushina <nastasya19.11@yandex.ru>
2023-06-06 22:52:10 +0300

    refactoring plain.js

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,29 +1,29 @@
-const formatValue = (value) => {
-  if (typeof value === 'object' && value !== null) {
+import _ from 'lodash';
+
+const formatValue = (value) => { // форматируем значение: если объект/строка/иное
+  if (_.isObject(value)) {
     return '[complex value]';
-  } if (typeof value === 'string') {
-    return `'${value}'`;
   }
-  return value;
+  return (typeof value === 'string') ? `'${value}'` : value;
 };
 
 const plain = (differences) => {
-  const iter = (data, name) => {
-    const result = data.flatMap((node) => {
-      const first = (Array.isArray(node.value)) ? `${node.name}.` : node.name;
-      const property = `Property '${name}${first}'`;
-      if (node.diff === 'deleted') {
-        return `${property} was removed`;
-      } if (node.diff === 'added') {
-        return `${property} was added with value: ${formatValue(node.value)}`;
-      } if (node.diff === 'unchanged') {
-        return [];
-      } if (Array.isArray(node.value)) {
-        return iter(node.value, `${name}${first}`);
-      }
-      return `${property} was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.value)}`;
-    });
-    return [...result].join('\n');
+  const iter = (data, pathOfName) => {
+    const result = data
+      .filter((node) => node.diff !== 'unchanged') // исключаем значения без изменений
+      .map((node) => {
+        const currentPathOfName = [...pathOfName, node.name]; // строим путь к свойству
+        const property = `Property '${currentPathOfName.join('.')}'`;
+        if (node.diff === 'deleted') {
+          return `${property} was removed`;
+        } if (node.diff === 'added') {
+          return `${property} was added with value: ${formatValue(node.value)}`;
+        } if (!Array.isArray(node.value)) {
+          return `${property} was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.value)}`;
+        }
+        return iter(node.value, currentPathOfName);
+      });
+    return result.join('\n');
   };
   return iter(differences, '');
 };
